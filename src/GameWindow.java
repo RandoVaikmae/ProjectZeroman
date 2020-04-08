ee041ca087b648112ec0cc1c52450ed924f09264
Rando Vaikmäe <randovaikmae2012@hotmail.com>
2020-04-08 17:15:49 +0300

    Collision tootab

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -7,28 +7,32 @@ import java.io.IOException;
 
 import javax.swing.JPanel;
 
-public class GameWindow extends JPanel{
+public class GameWindow extends JPanel {
 
 
     private static final long serialVersionUID = 1L;
-    public static final int WIDTH = 1020, HEIGHT= 640;
+    public static final int WIDTH = 1020, HEIGHT = 640;
 
-    public GameWindow(){
+    public GameWindow() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
 
     }
-    public void paint(Graphics g){
+
+    public void paint(Graphics g) {
         BufferedImage img = null;
         try {
             img = ImageIO.read(getClass().getResourceAsStream("/csgocar.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Color darkGreen = new Color(27,173,10);
-        g.clearRect(0,0,WIDTH,HEIGHT);
+        Color darkGreen = new Color(27, 173, 10);
+        g.clearRect(0, 0, WIDTH, HEIGHT);
         g.setColor(darkGreen);
         g.fillRect(0, 0, WIDTH, HEIGHT);
-        g.drawImage(img,200,200, 200, 200, null);
+        g.drawImage(img, 200, 200, 200, 200, null);
 
     }
+
+
 }
+
